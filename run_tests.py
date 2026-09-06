e9af931ca71ff1dccbf49ee688690d7af48c7165
Valerij Frolov <lamver@users.noreply.github.com>
2026-09-06 13:29:55 +0300

    Досчёт прошлой встречи больше не съедает речь новой

--- run_tests.py
+++ run_tests.py
@@ -35,6 +35,7 @@ FAST = [
     "backfill_test.py",
     "backfill_ui_test.py",
     "backfill_window_test.py",
+    "backfill_rivalry_test.py",
     "live_test.py",
     "langid_test.py",
     "voices_test.py",
